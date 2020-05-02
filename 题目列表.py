6de1340787384f9fc39eb619ed9f70c37d0f6a5f
shichenhang <181250124@smail.nju.edu.cn>
2020-05-02 10:59:37 +0800

    题目列表抽样

---
+++ 题目列表.py
@@ -0,0 +1,21 @@
+import json
+
+
+def getSampleCaseList():
+    f=open('test_data.json',encoding='utf-8')
+    res=f.read()
+    data=json.loads(res)
+
+    caseList=[]
+    for userId in data:
+        cases=data[userId]['cases']
+        for case in cases:
+            caseId=case['case_id']
+            if caseId not in caseList:
+                caseList.append(caseId)
+
+    sampleCaseList=[]
+    for i in range(0,len(caseList),6):
+        sampleCaseList.append(caseList[i])
+    f.close()
+    return sampleCaseList
