96110bc745d863699efdda577fcd424996923c7a
赵东辰 <>
2020-05-09 22:53:38 +0800

    平均行数，面向用例比例

---
+++ AverageLineNum.py
@@ -0,0 +1,185 @@
+import SampleCaseList
+import json
+import os
+from collections import defaultdict
+
+
+def getAverageLineNum():  # 返回一个字典case_id : averageLineNum
+    sampleCaseList = SampleCaseList.getSampleCaseList()
+    res = defaultdict(float)
+    # 所有数据的路径
+    data_path = os.getcwd() + "/data"
+    for case_id in sampleCaseList:
+        # caseXXXX的路径
+        cur_case_dir = data_path + "/case" + case_id
+        if not os.path.isdir(cur_case_dir):
+            print(cur_case_dir + "not found\n")
+
+        # 这里对一个case进行统计
+        res[case_id] = 0
+        count = 0
+        # 遍历该case下面的所有main.py文件
+        for user_files in os.listdir(cur_case_dir):
+            file_path = cur_case_dir + '/' + user_files + "/main.py"
+            if os.path.isfile(file_path):
+                with open(file_path, 'r') as f:
+                    res[case_id] += len(f.readlines())
+                # count += len(f.readlines())
+        res[case_id] /= len(os.listdir(cur_case_dir))
+        # print("case_id:", case_id + " average line num:", "%.2f" % res[case_id])
+    return res
+
+
+'''
+if __name__ == '__main__':
+    getAverageLineNum()
+'''
+'''结果先贴在这里
+case_id: 2908 average line num: 16.91
+case_id: 2456 average line num: 12.73
+case_id: 2892 average line num: 20.17
+case_id: 2894 average line num: 19.90
+case_id: 2171 average line num: 24.76
+case_id: 2145 average line num: 22.51
+case_id: 2185 average line num: 23.91
+case_id: 2182 average line num: 27.07
+case_id: 2618 average line num: 14.32
+case_id: 2395 average line num: 27.11
+case_id: 2425 average line num: 20.73
+case_id: 2466 average line num: 22.13
+case_id: 2209 average line num: 30.99
+case_id: 2283 average line num: 16.64
+case_id: 2359 average line num: 22.51
+case_id: 2278 average line num: 17.68
+case_id: 2461 average line num: 10.76
+case_id: 2542 average line num: 17.57
+case_id: 2505 average line num: 11.89
+case_id: 2441 average line num: 8.81
+case_id: 2111 average line num: 21.50
+case_id: 2096 average line num: 6.26
+case_id: 2127 average line num: 10.19
+case_id: 2584 average line num: 5.71
+case_id: 2064 average line num: 25.38
+case_id: 2134 average line num: 12.51
+case_id: 2084 average line num: 42.84
+case_id: 2174 average line num: 77.21
+case_id: 2296 average line num: 53.09
+case_id: 2149 average line num: 158.18
+case_id: 2288 average line num: 27.33
+case_id: 2415 average line num: 33.17
+case_id: 2311 average line num: 45.12
+case_id: 2139 average line num: 29.16
+case_id: 2790 average line num: 17.38
+case_id: 2809 average line num: 13.73
+case_id: 2827 average line num: 9.39
+case_id: 2846 average line num: 10.16
+case_id: 2810 average line num: 23.04
+case_id: 2817 average line num: 13.41
+case_id: 2801 average line num: 18.18
+case_id: 2796 average line num: 17.59
+case_id: 2867 average line num: 13.57
+case_id: 2843 average line num: 12.26
+case_id: 2849 average line num: 15.21
+case_id: 2855 average line num: 42.55
+case_id: 2830 average line num: 17.39
+case_id: 2838 average line num: 12.50
+case_id: 2857 average line num: 34.25
+case_id: 2921 average line num: 29.21
+case_id: 2884 average line num: 17.02
+case_id: 2928 average line num: 28.10
+case_id: 2923 average line num: 22.19
+case_id: 2910 average line num: 25.33
+case_id: 2917 average line num: 22.02
+case_id: 2909 average line num: 25.79
+case_id: 2896 average line num: 20.33
+case_id: 2934 average line num: 34.14
+case_id: 2192 average line num: 22.83
+case_id: 2667 average line num: 9.10
+case_id: 2662 average line num: 15.80
+case_id: 2656 average line num: 23.43
+case_id: 2430 average line num: 23.21
+case_id: 2468 average line num: 18.30
+case_id: 2729 average line num: 13.69
+case_id: 2282 average line num: 27.02
+case_id: 2737 average line num: 18.38
+case_id: 2220 average line num: 27.26
+case_id: 2230 average line num: 17.69
+case_id: 2227 average line num: 23.50
+case_id: 2123 average line num: 10.57
+case_id: 2215 average line num: 13.79
+case_id: 2157 average line num: 33.21
+case_id: 2375 average line num: 40.06
+case_id: 2405 average line num: 73.70
+case_id: 2458 average line num: 29.49
+case_id: 2432 average line num: 53.08
+case_id: 2538 average line num: 15.11
+case_id: 2513 average line num: 14.58
+case_id: 2516 average line num: 22.68
+case_id: 2210 average line num: 33.32
+case_id: 2688 average line num: 21.27
+case_id: 2287 average line num: 19.55
+case_id: 2480 average line num: 15.60
+case_id: 2676 average line num: 14.17
+case_id: 2951 average line num: 39.29
+case_id: 2938 average line num: 8.02
+case_id: 2251 average line num: 17.26
+case_id: 2325 average line num: 19.48
+case_id: 2246 average line num: 17.63
+case_id: 2242 average line num: 10.39
+case_id: 2216 average line num: 32.35
+case_id: 2376 average line num: 6.07
+case_id: 2232 average line num: 48.21
+case_id: 2213 average line num: 25.29
+case_id: 2644 average line num: 18.93
+case_id: 2641 average line num: 16.74
+case_id: 2639 average line num: 21.84
+case_id: 2399 average line num: 42.93
+case_id: 2351 average line num: 28.19
+case_id: 2369 average line num: 23.21
+case_id: 2556 average line num: 25.39
+case_id: 2553 average line num: 32.14
+case_id: 2663 average line num: 10.13
+case_id: 2590 average line num: 19.39
+case_id: 2487 average line num: 20.20
+case_id: 2725 average line num: 12.63
+case_id: 2559 average line num: 15.59
+case_id: 2730 average line num: 18.15
+case_id: 2974 average line num: 35.06
+case_id: 2969 average line num: 19.41
+case_id: 2945 average line num: 29.29
+case_id: 2235 average line num: 30.12
+case_id: 2374 average line num: 23.77
+case_id: 2532 average line num: 15.41
+case_id: 2524 average line num: 26.03
+case_id: 2648 average line num: 18.64
+case_id: 2657 average line num: 41.32
+case_id: 2598 average line num: 25.89
+case_id: 2398 average line num: 24.71
+case_id: 2250 average line num: 27.57
+case_id: 2324 average line num: 16.62
+case_id: 2409 average line num: 12.95
+case_id: 2362 average line num: 25.61
+case_id: 2377 average line num: 18.49
+case_id: 2745 average line num: 31.48
+case_id: 2717 average line num: 28.00
+case_id: 2573 average line num: 13.96
+case_id: 2768 average line num: 14.39
+case_id: 2764 average line num: 16.02
+case_id: 2779 average line num: 18.00
+case_id: 2476 average line num: 20.17
+case_id: 2980 average line num: 35.75
+case_id: 2976 average line num: 18.76
+case_id: 2948 average line num: 24.18
+case_id: 2742 average line num: 67.21
+case_id: 2713 average line num: 37.51
+case_id: 2776 average line num: 27.21
+case_id: 2714 average line num: 51.09
+case_id: 2623 average line num: 8.20
+case_id: 2585 average line num: 22.24
+case_id: 2583 average line num: 23.60
+case_id: 2613 average line num: 21.15
+case_id: 2619 average line num: 24.22
+case_id: 2750 average line num: 32.00
+case_id: 2752 average line num: 44.17
+case_id: 2422 average line num: 16.66
+'''
---
+++ cheatRatio.py
@@ -0,0 +1,176 @@
+import SampleCaseList
+import json
+import os
+from collections import defaultdict
+import re
+
+
+def cheatRatio():
+
+    """
+    统计每道题目面向用例的比例
+    # （暂未生效）假设： 所有面向用例的题目均为满分
+    # 思路：两方面的分析，其一是提交的分数占据所有分数的程度，其二是代码中是否出现 if == print结构
+    # （暂未生效）假设： 用例个数不超过3个的题目只能用第二种方法判断
+    # 更正: 目前只用第二种方法来判断
+    # 第二种方法思路：
+    # 1、查找 if ｜ elif ｜ else  +  ==  +  print 的结构
+    # 2、统计这种结构在单个代码中出现的次数
+    # （暂未实施）统计这种结构代码行数在整体中所占比例
+    # 3、这种结构的个数大于等于3，判定面向用例
+    # 未来可以扩充的部分：1、查找得分情况，根据得分综合判断  2、代码文本匹配，假设先提交者为原创  3、过滤不是python的代码
+    :return: 返回字典， case_id : cheat_Ratio 对应面向用例的比例
+    """
+    sampleCaseList = SampleCaseList.getSampleCaseList()
+
+    is_cheat_judge_1 = defaultdict(float)
+    is_cheat_judge_2 = defaultdict(float)
+    all_scores = defaultdict(list)  # 储存的是case_id : list(list)，是一个case的所有人的各次提交分数
+    for case_id in sampleCaseList:
+        is_cheat_judge_1[case_id] = 0
+        is_cheat_judge_2[case_id] = 0
+        all_scores[case_id] = []
+
+    # 第一部分
+    f = open('test_data.json', encoding='utf-8')
+    res = f.read()
+    data = json.loads(res)
+
+    # 收集每道题目满分的所有score
+    for user_id in data:
+        cases = data[user_id]['cases']
+        for case in cases:
+            cur_case_id = case['case_id']
+            if cur_case_id in sampleCaseList:
+                if case['final_score'] == 100:
+                    scores = []
+                    for upload_record in case['upload_records']:
+                        scores.append(upload_record['score'])
+                    all_scores[cur_case_id].append(scores)
+
+    # 对每道题目所有分数进行分析
+    for case in all_scores.keys():
+        case_scores = all_scores[case]
+        temp = analyse_scores(case_scores)
+        # print("case_id: ", case, "ratio: ", temp)
+
+    # 第二部分
+
+    # 所有数据的路径
+    data_path = os.getcwd() + "/data"
+    for case_id in sampleCaseList:
+
+        user_matches = defaultdict(int)
+
+        # caseXXXX的路径
+        cur_case_dir = data_path + "/case" + case_id
+        if not os.path.isdir(cur_case_dir):
+            print(cur_case_dir + "not found\n")  # 多打一个空行，调试的时候方便发现
+        for user_files in os.listdir(cur_case_dir):
+            user_matches[user_files] = 0
+            file_path = cur_case_dir + '/' + user_files + "/main.py"
+            if os.path.isfile(file_path):
+                with open(file_path, 'r') as f:
+                    total_matches = 0
+                    lines = f.readlines()
+                    # for line_num in range(0, len(lines)):
+                    line_ptr = 0
+                    while True:
+                        if line_ptr >= len(lines):
+                            break
+                        cur_line = lines[line_ptr]  # debug用
+                        one_try_res = match_pattern(lines, line_ptr)
+                        total_matches += one_try_res[0]
+                        line_ptr = one_try_res[1]
+                    # print(total_matches)
+                    user_matches[user_files] = total_matches
+        # print(case_id, user_matches)
+
+        cheat_count_for_one_case = 0
+        for matched_pattern in user_matches.values():
+            if matched_pattern >= 3:
+                cheat_count_for_one_case += 1
+        is_cheat_judge_2[case_id] = cheat_count_for_one_case / len(user_matches.values())
+        # print("case_id: ", case_id, "cheats: " ,cheat_count_for_one_case, "total users: ", len(user_matches.values()))
+    # print(is_cheat_judge_2)
+
+
+
+def analyse_scores(scores: list) -> float:
+    """
+    分析scores数组，返回该数组所对应的面向用例的比例
+    大致过程：先根据最小分数间距找到这道题目的用例个数，然后判断有没有人从0分逐步递增到100分，有的话判断面向用例
+    这个方法暂时不用，因为发现很多实际面向用例，但是直接100分的代码
+    :param scores: 某case_id下所有的分数组，其中每个分数组对应一个user的所有次提交的分数
+    :return: 该case_id的面向用例的比例
+    """
+    res = 0.0
+    cheat_num = 0
+    # 首先根据所有人的结果，找出这道题目的用例个数
+    min_gap = 100
+    for score in scores:  # 这个时候score仍然是一个数组，表示一个人对所有提交
+        for i in range(1, len(score)):
+            if min_gap > score[i] - score[i - 1] > 1:  # 大于1是为了防止某些可能的特殊情况
+                min_gap = score[i] - score[i - 1]
+    test_case_num = round(100 / min_gap)
+    if test_case_num <= 3:
+        return 0.0
+
+    cheat_count_judge1 = 0
+    for score in scores:
+
+        set_score = list(set(score))
+        set_score.sort()
+        # if set_score[0] != 0: #第一次提交就不是零分，一定不会面向用例
+        #  continue
+        min_gap_num = 0  # 记录一个人的所有记录得分的间距情况
+        for i in range(1, len(set_score)):
+            if abs((set_score[i] - set_score[i - 1]) - min_gap) < 1:
+                min_gap_num += 1
+        if min_gap_num == test_case_num:  # 每个用例都尝试过，判定面向用例
+            print(score)
+            cheat_num += 1
+    return cheat_num / len(scores)
+
+
+def match_pattern(lines: list, cur_pos):  # 判断有多少个那种结构
+    """
+    从cur_pos开始，找到下一个"if  == print"结构，返回该结构的下一行
+    :param lines: 某一user的代码
+    :param cur_pos: 代码行指针，代码行从0算起
+    :return: 数组，第一个元素是"if == print"的结构个数（在第cur_pos行和第line_ptr行之间的个数），第二个元素是当前代码行指针的位置
+    """
+    res = [0, cur_pos]
+    re1 = re.compile('(if|elif)')
+    re2 = re.compile('(==|operator.eq)')
+    re3 = re.compile('print\(')
+    re4 = re.compile('else')
+    line_ptr = cur_pos
+    max_lines = len(lines)
+    cur_line = lines[line_ptr]
+    if re4.search(cur_line) or (re1.search(cur_line) and re2.search(cur_line)):
+        if re3.search(cur_line):
+            line_ptr += 1
+            res[0] += 1
+            res[1] = line_ptr
+            return res
+        line_ptr += 1
+        while True:
+            if line_ptr >= max_lines:
+                res[1] = line_ptr
+                return res
+            cur_line = lines[line_ptr]
+            if re4.search(cur_line) or (re1.search(cur_line) and re2.search(cur_line)):
+                res[1] = line_ptr
+                return res
+            if re3.search(cur_line):
+                res[0] += 1
+                res[1] = line_ptr + 1
+                return res
+            line_ptr += 1
+    res[1] = line_ptr + 1
+    return res
+
+
+# if __name__ == '__main__':
+#    cheatRatio()
