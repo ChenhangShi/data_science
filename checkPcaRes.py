da3f3f3183912994c086356b83a071fd006aefb8
181250178@smail.nju.edu.cn <181250178@smail.nju.edu.cn>
2020-07-08 15:48:19 +0800

    调整pca数据存取位置

--- checkPcaRes.py
+++ checkPcaRes.py
@@ -4,36 +4,39 @@ from pcaDataVisualization import do_visualization
 
 
 def check_score(res):
-    # 测试的比较简单的题目，发现都是小于0的，符合预期
-    print(2425, res['2425'])
-    print(2388, res['2388'])
-    print(2386, res['2386'])
-    print(2280, res['2280'])
-    print(2358, res['2358'])
-    print(2278, res['2278'])
-    print(2453, res['2453'])
-    print(2122, res['2122'])
-    print(2063, res['2063'])
-    print(2847, res['2847'])
-    print(2816, res['2816'])
-    print(2801, res['2801'])
-    print('\n')
-
-    # 测试的比较难的题目,发现都是大于0.6的，符合预期
-    # 2301的作弊率非常高，因而算出来的结果也很大，虽然代码行数、花费时间都很小，但综合算出来的结果仍然很大，符合预期
-    print(2301, res['2301'])
-    print(2310, res['2310'])
-    print(2954, res['2954'])
-    print(2969, res['2969'])
-    print(2348, res['2348'])
-    print(2588, res['2588'])
-    print(2947, res['2947'])
-    print(2887, res['2887'])
-    print(2091, res['2091'])
-    print(2383, res['2383'])
-    print(2273, res['2273'])
-    print(2085, res['2085'])
-    print('\n\n\n')
+    easyDic = defaultdict(list)
+    hardDic = defaultdict(list)
+    easyDic['2425'] = res['2425']
+    easyDic['2388'] = res['2388']
+    easyDic['2386'] = res['2386']
+    easyDic['2280'] = res['2280']
+    easyDic['2358'] = res['2358']
+    easyDic['2278'] = res['2278']
+    easyDic['2453'] = res['2453']
+    easyDic['2122'] = res['2122']
+    easyDic['2063'] = res['2063']
+    easyDic['2847'] = res['2847']
+    easyDic['2816'] = res['2816']
+    easyDic['2801'] = res['2801']
+    hardDic['2301'] = res['2301']
+    hardDic['2310'] = res['2310']
+    hardDic['2954'] = res['2954']
+    hardDic['2969'] = res['2969']
+    hardDic['2348'] = res['2348']
+    hardDic['2588'] = res['2588']
+    hardDic['2947'] = res['2947']
+    hardDic['2887'] = res['2887']
+    hardDic['2091'] = res['2091']
+    hardDic['2383'] = res['2383']
+    hardDic['2273'] = res['2273']
+    hardDic['2085'] = res['2085']
+    test_dict = {
+        'easyDic': easyDic,
+        'hardDic': hardDic
+    }
+    json_str = json.dumps(test_dict, indent=4)
+    with open('easyData.json', 'w') as json_file:
+        json_file.write(json_str)
 
 
 def check_distribution(res, another_res):
--- pca.py
+++ pca.py
@@ -135,39 +135,6 @@ def myPCA(X, case_id_list):
     res = dict(zip(case_id_list, res))
     print('res:')
     print(res)
-    easyDic=defaultdict(list)
-    hardDic=defaultdict(list)
-    easyDic['2425']=res['2425']
-    easyDic['2388']=res['2388']
-    easyDic['2386'] = res['2386']
-    easyDic['2280'] = res['2280']
-    easyDic['2358'] = res['2358']
-    easyDic['2278'] = res['2278']
-    easyDic['2453'] = res['2453']
-    easyDic['2122'] = res['2122']
-    easyDic['2063'] = res['2063']
-    easyDic['2847'] = res['2847']
-    easyDic['2816'] = res['2816']
-    easyDic['2801'] = res['2801']
-    hardDic['2301']=res['2301']
-    hardDic['2310'] = res['2310']
-    hardDic['2954'] = res['2954']
-    hardDic['2969'] = res['2969']
-    hardDic['2348'] = res['2348']
-    hardDic['2588'] = res['2588']
-    hardDic['2947'] = res['2947']
-    hardDic['2887'] = res['2887']
-    hardDic['2091'] = res['2091']
-    hardDic['2383'] = res['2383']
-    hardDic['2273'] = res['2273']
-    hardDic['2085'] = res['2085']
-    test_dict={
-        'easyDic':easyDic,
-        'hardDic':hardDic
-    }
-    json_str = json.dumps(test_dict, indent=4)
-    with open('easyData.json', 'w') as json_file:
-        json_file.write(json_str)
     print('\n\n\n')
     # 要返回的：
     # 综合结果res
