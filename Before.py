4107eddb401f46f084feb6dcb5abd6abbfd00e6c
shichenhang <181250124@smail.nju.edu.cn>
2020-07-13 17:25:43 +0800

    修正注释

--- Before.py
+++ Before.py
@@ -17,6 +17,4 @@ if not os.path.exists('data'):
 # 在其他模块中序列化，序列化后的开头为"c"+类所在的模块的名称，知道去哪里找。
 # 但如果是远程传输，远程没有该模块，则应该在本模块中序列化，使模块名为__main__，在远程反序列化的模块里复制该类的定义（类如果有方法，方法名要相同，方法体可以不相同，因为不序列化方法体）
 serialize_all_case_objs()
-
-# PcaModel类不在该函数所在的模块，所以即使在SaveTrainingResult的main中运行，也不用import
 serialize_training_result()
